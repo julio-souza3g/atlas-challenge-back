6326a1b556870c9c03ec9197d7d42fea1df9ebcd
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 04:36:55 -0300

    fix: set mongo url to memory tests and db atlas

--- src/database/mongodb/mongo-helper.ts
+++ src/database/mongodb/mongo-helper.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 export const MongoHelper = {
   client: undefined,
@@ -6,24 +6,28 @@ export const MongoHelper = {
 
   async connect(uri?: string): Promise<void> {
     if(uri) {
-      this.uri = uri
+      this.uri = uri;
+      this.client = await mongoose.connect(this.uri, {
+        dbName: "atlas_challenge_db",
+      });
+    } else {
+      this.client = await mongoose.connect(process.env.MONGO_URL);
     }
-    this.client = await mongoose.connect(this.uri ? this.uri : process.env.MONGO_URL)
   },
 
   async disconnect(): Promise<void> {
-    await mongoose.connection.close()
-    this.uri = null as string
-    this.client = undefined
+    await mongoose.connection.close();
+    this.uri = null as string;
+    this.client = undefined;
   },
 
   async clearCollection(collectionName: string): Promise<void> {
-    const collection = mongoose.connection.collection(collectionName)
-    await collection.deleteMany({})
+    const collection = mongoose.connection.collection(collectionName);
+    await collection.deleteMany({});
   },
 
-  map (collection: any): any {
-    const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, collectionWithoutId, { id: _id.toString() })
-  }
-}
+  map(collection: any): any {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id.toString() });
+  },
+};
