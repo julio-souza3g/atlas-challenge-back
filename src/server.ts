0d83394cae9b25bfa891862665bc10b25def1e03
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 03:57:49 -0300

    feat(src/server.ts): add mongo connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-import app from "./app";
+import 'dotenv/config';
+import { MongoHelper } from "./database/mongodb/mongo-helper";
 
-app.listen(3333, () => {
-  console.log("Server started on port 3333!");
+MongoHelper.connect(process.env.MONGO_ATLAS_URL).then(async () => {
+  const app = (await import('./app')).default
+  app.listen(process.env.PORT || 4000, () => {
+    console.log('Server running on port', process.env.PORT || 4000);
+  });
+}).catch((error) => {
+  console.error(error);
 });
