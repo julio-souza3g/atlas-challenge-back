6784b4f9996dd4e0d38114df4fa3c080d805f813
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 02:54:43 -0300

    feat(src/database/mongodb): add mongo helper

---
+++ src/database/mongodb/mongo-helper.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+export const MongoHelper = {
+  client: undefined,
+  uri: null as string,
+
+  async connect(uri?: string): Promise<void> {
+    if(uri) {
+      this.uri = uri
+    }
+    this.client = await mongoose.connect(this.uri ? this.uri : process.env.MONGO_URL)
+  },
+
+  async disconnect(): Promise<void> {
+    await mongoose.connection.close()
+    this.uri = null as string
+    this.client = undefined
+  },
+
+  async clearCollection(collectionName: string): Promise<void> {
+    const collection = mongoose.connection.collection(collectionName)
+    await collection.deleteMany({})
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id.toString() })
+  }
+}
