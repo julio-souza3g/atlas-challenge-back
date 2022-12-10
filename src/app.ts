0f878b2057547e13f350e60254a4704228b3cc97
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 11:15:42 -0300

    feat: add swagger to generate documentation from api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger.json';
 import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 export default app;
