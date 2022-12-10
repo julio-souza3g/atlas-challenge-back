f8ae26ca60cb16e8cd7055dabb2b05fec4a4e1e5
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 03:57:20 -0300

    feat(src/routes.ts): add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { AuthUserController } from './controllers/auth-user-controller';
+import { ListContactsController } from './controllers/list-contacts-controller';
 
 const routes = Router();
+
+const authUserController = new AuthUserController();
+const listContactsController = new ListContactsController();
+
+routes.post('/login', authUserController.handle );
+routes.get('/list-friends', listContactsController.handle );
 
 export default routes;
